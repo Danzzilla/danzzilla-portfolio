8481de35e62f14a02486eca37fc1ed8159d45336
Daniel Svirida <danielsvirida@gmail.com>
2023-09-25 19:37:56 -0700

    Navbar styles created

--- scripts/styling.js
+++ scripts/styling.js
@@ -1,25 +1,21 @@
-//Document Object Declarations
-const photography = document.querySelector('#photography');
-const videography = document.querySelector('#videography');
-const background = document.querySelector('#home-wrapper');
+const navbar = document.querySelector("#nav-wrapper");
+const logo = document.querySelector("#logo");
+const navtoggle = document.querySelector(".navbar-toggler");
+const nav = document.querySelector(".navbar-nav");
+window.addEventListener("scroll", (event) => {
+    let scrollval = $(window).scrollTop();
+    let navopacityadj = -200 / (scrollval + 14.2857) + 1;
+    let navitemopacityadj = -400 / (scrollval + 14.2857) + 1;
 
-//Event Listeners
-photography.addEventListener('mouseover', photographyView);
-videography.addEventListener('mouseover', videographyView);
-photography.addEventListener('mouseout', defaultView);
-videography.addEventListener('mouseout', defaultView);
+    navbar.setAttribute("style", "background-color: rgba(60, 100, 77, " + navopacityadj + ");"); //133, 143, 130,
+    logo.setAttribute("style", "background-color: rgba(241, 241, 241, " + navitemopacityadj + ");"); //221, 222, 224,
+    // if(scrollval > 300){
+    //     logo.setAttribute("style", "width: " + 150 - (0.05 * scrollval) + " px;");
+    //     console.log(150 - (0.1 * scrollval));
+    // }
+    navtoggle.setAttribute("style", "background-color: rgba(241, 241, 241, " + navitemopacityadj + ");");
+    nav.setAttribute("style", "background-color: rgba(241, 241, 241, " + navitemopacityadj + ");");
 
-//Functions
-function defaultView(){
-    background.style.backgroundImage = 'url("images/website/main.jpg")';
-}
-
-function photographyView(){
-    background.style.backgroundImage = 'url("images/website/back.jpg")';
-}
-
-function videographyView(){
-    background.style.backgroundImage = 'url("images/website/gas.jpg")';
-}
-
-//TODO add a timer between each crossfade
+    // console.log(scrollval);
+    // console.log(-200 / ((scrollval + 14.2857)) + 1);
+})
