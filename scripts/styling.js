d8aeb58784ddcad162abd880b69cc5d231a75f13
Daniel Svirida <danielsvirida@gmail.com>
2023-10-08 20:32:14 -0700

    Portfolio Carousel created

--- scripts/styling.js
+++ scripts/styling.js
@@ -28,4 +28,74 @@ window.addEventListener("scroll", (event) => {
 
 const about = document.querySelector("#about");
 about.setAttribute("style", "margin-top: " + $(window).height() + "px");
-console.log($(window).height());
+
+// carousel //
+
+const carouselContainer = document.querySelector('.mycarousel-container');
+const carouselControlsContainer = document.querySelector('.mycarousel-controls');
+const carouselControls = ['previous', 'next'];
+const carouselItems = document.querySelectorAll('.mycarousel-item');
+
+class Carousel{
+
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateCarousel(){
+        this.carouselArray.forEach(el => {
+            el.classList.remove('mycarousel-item-1');
+            el.classList.remove('mycarousel-item-2');
+            el.classList.remove('mycarousel-item-3');
+            el.classList.remove('mycarousel-item-4');
+            el.classList.remove('mycarousel-item-5');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`mycarousel-item-${i+1}`);
+        })
+    }
+
+    setCurrentState(direction){
+        if(direction.className === 'mycarousel-controls-previous'){
+            this.carouselArray.unshift(this.carouselArray.pop());
+        }
+        else{
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateCarousel();
+    }
+
+    setControls(){
+        this.carouselControls.forEach(control => {
+            carouselControlsContainer.appendChild(document.createElement('button')).className
+                = `mycarousel-controls-${control}`;
+        });
+    }
+
+    useControls(){
+        const triggers = [...carouselControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+               e.preventDefault();
+               this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const carousel = new Carousel(carouselContainer, carouselItems, carouselControls);
+carousel.setControls();
+carousel.useControls();
+let carouselInterval = setInterval(function(){
+    document.querySelector('.mycarousel-controls-next').click();}, 5000);
+
+document.querySelector('.mycarousel-controls-previous').addEventListener('click', () => {
+    clearInterval(carouselInterval);
+    setTimeout(function(){
+        carouselInterval = setInterval(function(){
+            document.querySelector('.mycarousel-controls-next').click();}, 5000);
+    }, 10000);
+})
