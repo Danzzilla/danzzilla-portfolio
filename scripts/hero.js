c07ff1d07a512603bb7578a0b104aad7e29d4348
Daniel Svirida <danielsvirida@gmail.com>
2025-04-09 09:25:58 -0700

    Added HDRI

--- scripts/hero.js
+++ scripts/hero.js
@@ -1,43 +1,56 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-let camera;
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// camera.position.z = 2;
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 2;
 
-let object;
+let model;
 let controls;
 
-const loader = new GLTFLoader();
-loader.load(
-  `../models/garage/1.gltf`,
-  function(gltf){
-      object = gltf.scene;
-      camera = gltf.cameras[0];
-      object.scale.setScalar(50);
-      scene.add(object);
-  },
-    function(xhr){
-      console.log((xhr.loaded / (xhr.total * 100)) + "% Loaded");
-    },
-    function(error){
-      console.error(error);
+new RGBELoader().load(
+    "../models/garage/symmetrical_garden_02_2k.hdr",
+    function(texture){
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        scene.environment = texture;
+        scene.background = texture;
+
+        animate();
+
+        new GLTFLoader().load(
+            `../models/garage/1.gltf`,
+            async function(gltf){
+                model = gltf.scene;
+                camera = gltf.cameras[0];
+
+                await renderer.compileAsync(model, camera, scene);
+                scene.add(model);
+
+                animate();
+            },
+            function(xhr){
+                console.log((xhr.loaded / (xhr.total * 100)) + "% Loaded");
+            },
+            function(error){
+                console.error(error);
+            }
+        );
     }
 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
 document.getElementById("canvas").appendChild(renderer.domElement);
 
-const topLight = new THREE.DirectionalLight(0xffffff, 1);
-topLight.position.set(500, 500, 500);
-topLight.castShadow = true;
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, 5);
-scene.add(ambientLight);
+const sun = new THREE.DirectionalLight(0xfff2e5, 1);
+sun.position.set(10, 10, 10)
+sun.castShadow = true;
+scene.add(sun);
 
 function animate(){
     requestAnimationFrame(animate);
