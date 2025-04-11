b3fc760236163708dd2b1cb8484ff90ccebb7283
Daniel Svirida <danielsvirida@gmail.com>
2025-04-11 13:22:54 -0700

    Added EffectComposer

--- scripts/hero.js
+++ scripts/hero.js
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
@@ -8,15 +11,18 @@ let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.outputMapping = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.CineonToneMapping;
+renderer.toneMappingExposure = 0.5;
 renderer.gammaOutput = true;
 renderer.gammaFactor = 2.2;
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.getElementById("canvas").appendChild(renderer.domElement);
 
 new RGBELoader().load(
-    "../models/garage/symmetrical_garden_02_2k.hdr",
+    "../models/garage/blocky_photo_studio_2k.hdr",
     function(texture){
         texture.mapping = THREE.EquirectangularReflectionMapping;
         texture.encoding = THREE.sRGBEncoding;
@@ -52,7 +58,7 @@ new RGBELoader().load(
     }
 );
 
-const sun = new THREE.DirectionalLight(0xfff2e5, 10);
+const sun = new THREE.DirectionalLight(0xfff2e5, 5);
 sun.position.set(5, 35, 8)
 sun.castShadow = true;
 sun.shadow.camera.top += 25;
@@ -61,14 +67,24 @@ sun.shadow.camera.right += 25;
 sun.shadow.camera.left -= 25;
 sun.shadow.camera.near = 25;
 sun.shadow.camera.far = 150;
-sun.shadow.bias = -0.003;
+sun.shadow.bias = -0.002;
 sun.shadow.mapSize.width = 4096;
 sun.shadow.mapSize.height = 4096;
 scene.add(sun);
 
+const composer = new EffectComposer(renderer);
+composer.addPass(new RenderPass(scene, camera));
+
+composer.addPass(new OutputPass());
+
+
 function animate(){
+    let time = Date.now() * 0.005;
+    sun.position.y = Math.sin(time * 0.07) * 35;
+    sun.position.z = Math.sin(time * 0.07) * 35;
+
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    composer.render();
 }
 
 window.addEventListener("resize", function(){
